3847e9bcf1b741c51548d89d7e714838849a5e53
SmartOSC-Luonglb <105040114+luonglb33@users.noreply.github.com>
2024-11-25 17:39:30 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+// Middleware to parse incoming JSON
+app.use(express.json());
+
+app.post("/cybersource/callback", (req, res) => {
+  console.log("🚀 ~ app.post ~ req.body:", req.body);
+
+  // if (decision === "ACCEPT") {
+  //   console.log(`Token received: ${payment_token}`);
+  //   console.log(`Card Details: ${JSON.stringify(card)}`);
+  //   // Save the token and card details to your database for future transactions
+  // } else {
+  //   console.error(`Error: ${message}`);
+  // }
+
+  // Respond to CyberSource
+  res.status(200).send("Received");
+});
+
+app.get("/", (req, res) => {
+  console.log("🚀 ~ app.get ~ req:", req);
+  res.send("Hello World");
+});
+
+app.listen(8085, () => {
+  console.log("Server running on http://localhost:8085");
+});
