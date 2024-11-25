e3458bb15c52429f383dad044ee9394a07fe9b2b
SmartOSC-Luonglb <105040114+luonglb33@users.noreply.github.com>
2024-11-25 17:49:25 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const app = express();
 app.use(express.json());
 
 app.post("/cybersource/callback", (req, res) => {
-  console.log("🚀 ~ app.post ~ req.body:", req.body);
+  console.log("🚀 ~ app.post ~ req.body:", req.params);
+  console.log("🚀 ~ app.post ~ req.body:", req.query);
 
   // if (decision === "ACCEPT") {
   //   console.log(`Token received: ${payment_token}`);
